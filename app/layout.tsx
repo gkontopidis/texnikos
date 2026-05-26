7e0e9a5323d71a2e2a5cdd4c00c943396a5eab64
almend <262774640+kontopidisg@users.noreply.github.com>
2026-05-26 10:27:06 +0300

    proxy

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,12 +4,12 @@ import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
-  subsets: ["latin", "greek"],
+  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
-  subsets: ["latin", "greek"],
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
